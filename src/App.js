43c291353a2038965a1546ebd47c13140f54630e
wondwosen shiferaw <wondwosen.shi@gmail.com>
2020-11-03 00:24:00 +0100

    Created custom hook for fetch data.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,60 @@
 import React from "react";
-import axios from "axios";
 import "./App.css";
 import Cat from "./components/Cat";
 import Dog from "./components/Dog";
 import Mouse from "./components/Mouse";
 import Head from "./components/Head";
+import useRequest from "./components/useRequest";
 import { UserProvider } from "./context/userContext";
 import Posts from "./components/Posts";
 import NavBar from "./components/NavBar";
-const users = [
-  { id: 1, fName: "wonde", lName: "Tsige", age: "30" },
-  {
-    id: 2,
-    fName: "Abel",
-    lName: "Shi",
-    age: "20",
-  },
-  {
-    id: 3,
-    fName: "Meron",
-    lName: "Awol",
-    age: "25",
-  },
-];
-function App() {
-  const [posts, setPost] = React.useState([]);
-  const [error, setError] = React.useState("");
 
-  React.useEffect(() => {
-    async function fetchPost() {
-      try {
-        const response = await axios.get(
-          "http://jsonplaceholder.typicode.com/posts?_limit=10"
-        );
-        if (response) {
-          const postData = await response.data;
-          setPost([...postData]);
-        }
-      } catch (e) {
-        console.log(e);
-        setError("Error retrieving Data");
-      }
-    }
-    fetchPost();
-  }, []);
+function App() {
+  // const [posts, setPost] = React.useState([]);
+  // const [error, setError] = React.useState("");
+  const { posts, error, loading } = useRequest(
+    "http://jsonplaceholder.typicode.com/posts?_limit=10"
+  );
+
+  // async function fetchPost() {
+  //   try {
+  //     const response = await axios.get(
+  //       "http://jsonplaceholder.typicode.com/posts?_limit=10"
+  //     );
+  //     if (response) {
+  //       const postData = await response.data;
+  //       setPost([...postData]);
+  //     }
+  //   } catch (e) {
+  //     setError("Error retrieving Data");
+  //   }
+  // }
+
+  // React.useEffect(() => {
+  //   fetchPost();
+  // }, []);
   return (
     <div className='App'>
-      <UserProvider value={{ users, posts, error }}>
-        <Head />
-        <NavBar />
-        <div className='app-body'>
-          <div className='app-container'>
-            <Cat name='Perry' />
-            <Mouse
-              render={(mouse, handleCount, resetCount) => (
-                <Dog
-                  count={mouse}
-                  handleCount={handleCount}
-                  resetCount={resetCount}
-                />
-              )}
-            />
+      <UserProvider value={{ posts, error }}>
+        <>
+          <Head />
+          <NavBar />
+          <div className='app-body'>
+            <div className='app-container'>
+              <Cat name='Perry' />
+              <Mouse
+                render={(mouse, handleCount, resetCount) => (
+                  <Dog
+                    count={mouse}
+                    handleCount={handleCount}
+                    resetCount={resetCount}
+                  />
+                )}
+              />
+            </div>
+            {loading || error ? error : <Posts />}
           </div>
-          <Posts />
-        </div>
+        </>
       </UserProvider>
     </div>
   );
---
+++ src/components/useRequest.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useRequest = (initUrl) => {
+  const [posts, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    let ignore = false;
+    const fetchProduct = async () => {
+      try {
+        setLoading(true);
+        const response = await axios(initUrl);
+        if (!ignore) setData(response.data);
+      } catch (err) {
+        setError("Error Fetching Data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProduct();
+
+    return () => {
+      ignore = true;
+    };
+  }, [initUrl]);
+  return { posts, loading, error };
+};
+export default useRequest;
