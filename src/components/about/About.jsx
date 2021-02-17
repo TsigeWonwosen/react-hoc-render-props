f0ab66ef4748562f0a6105a0e087535ae250c6aa
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-02-17 09:27:07 +0100

    About page Edited.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,9 +19,9 @@ function About() {
         <Paragraph>
           I am a Tsige Wondwosen shiferaw a Fullstack developer by profession
           with formal education in computer science (Master's from Politecnico
-          di Torino). Currently i am looking full time position. Through my
+          di Torino). Currently i am looking full time position. Through out my
           education and work experience a learned a lot how to create a web site
-          front end to back end. Currently i am working on my owen projects.
+          front end to back end. Currently i am working on my own projects.
           <br />
           Here is a list of technologies that I use in my work: JavaScript(ES6)
           , React , Node , Mongoose, Redux , Gatsby , Sass , Styled
